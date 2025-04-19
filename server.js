cf84b8a674912e31dfd7b7129a4b08feca9112d4
toey-jsd9 <toeykenji@gmail.com>
2025-04-19 20:29:59 +0700

    fixfix

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const mongoose = require("mongoose");
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected!"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
 
 const personSchema = new mongoose.Schema({
   name: String,
